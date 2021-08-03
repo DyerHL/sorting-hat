45006329c6fced853810661b83e813ebe6be20ec
Halie Dyer <Halie.L.Dyer@gmail.com>
2021-08-02 20:22:23 -0500

    added welcome card, sumbit button populates form

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log ("Lets make a sorting hat")
+let studentsArrays = [];
+
+const renderToDom = (divId, textToPrint) => {
+    const selectedDiv = document.querySelector(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const loadForm = () => {
+    let form = `
+        <div class="form">
+          <input type="text" class="form-control" placeholder="Student's Name" aria-label="Recipient's username" aria-describedby="button-addon2">
+          <button class="btn btn-outline-secondary" type="button" id="btn sortMe">Sort Me</button>
+        </div>
+    `;
+    renderToDom("#formHolder", form);
+};
+
+
+
+
+
+
+
+
+
+
+
+const buttonEvents = () => {
+    document.querySelector('#sortButton').addEventListener("click",loadForm); /*makes sort button open form*/
+    document.querySelector('#btn sortMe').addEventListener('click',); /*makes student card*/
+}
+
+const loadPage = () => {
+    buttonEvents();
+};
+
+loadPage();
